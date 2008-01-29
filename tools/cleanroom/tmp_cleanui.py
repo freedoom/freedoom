6b363e237a9fac02f9869a3b4e592113990f621e
Jon Dowland <jon@alcopop.org>
2008-01-29 18:21:37 +0000

    copy a texture definition and it's patches into this dir

--- tools/cleanroom/tmp_cleanui.py
+++ tools/cleanroom/tmp_cleanui.py
@@ -6,12 +6,21 @@ import gtk.glade
 
 class HellowWorldGTK:
 	"""This is an Hello World GTK application"""
+	
+	example_texture = """
+COMPUTE2                256     56
+*       COMP02_1        0       0
+*       COMP02_2        64      0
+*       COMP02_3        128     0
+*       COMP02_7        192     0
+	"""
+	
 	def __init__(self):
 		self.gladefile = "cleanroom.glade"
 		self.wTree = gtk.glade.XML(self.gladefile,"window1")
 		
 		self.image1 = self.wTree.get_widget("orig_texture")
-		self.image1.set_from_file("sw2_1.gif")
+		self.image1.set_from_file("comp02_1.gif")
 		pixbuf = self.image1.get_pixbuf()
 		if pixbuf:
 			scale = 3
