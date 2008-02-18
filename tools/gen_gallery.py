e9335e30d6997d6d00f2d5ac3773e2f95111fbc4
Jon Dowland <jon@alcopop.org>
2008-02-18 19:34:06 +0000

    gen_gallery.py: quick hack to generate HTML gallery of patches

---
+++ tools/gen_gallery.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+import os,sys,re
+
+# this sucks
+
+patches = [ x for x in os.listdir('.') if re.match(r'.*\.gif$', x) ]
+
+print '''<style type="text/css">
+	div {
+		float: left;
+		width: 20%;
+	}
+</style>
+'''
+print ''.join(['<div><img src="%s" /><br />%s</div>' % (x,x) for x in patches])
