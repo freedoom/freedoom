758c53fb542c6b9383341769094f1e8910571e60
Ayub Ahmed <ayubahmed2240@gmail.com>
2017-03-11 21:05:25 +0600

    text/config.py: make t_phase images white

--- graphics/text/config.py
+++ graphics/text/config.py
@@ -64,6 +64,10 @@ white_graphics = {
 	'wibp3': 'P3',
 	'wibp4': 'P4',
 	'wicolon': ':',
+	
+	# These files are for the title screens of Phase 1 and Phase 2
+	't_phase1': 'PHASE 1',
+	't_phase2': 'PHASE 2',
 
 	# Note: level names are also included in this dictionary, with
 	# the data added programatically from the DEHACKED lump, see
@@ -75,10 +79,6 @@ blue_graphics = {
 	'm_episod': 'Choose Chapter:',
 	'm_optttl': 'OPTIONS',
 	'm_skill': 'Choose Skill Level:',
-	
-	# These files are for the title screens of Phase 1 and Phase 2
-	't_phase1': 'PHASE 1',
-	't_phase2': 'PHASE 2',
 }
 
 red_graphics = {
