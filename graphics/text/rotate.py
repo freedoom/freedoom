7251b148920917f688f5cda266b5bbabdba34301
Nick Zatkovich <NZatkovich@gmail.com>
2017-07-31 02:01:48 -0700

    rotate: fix rotate
    Image.rotate had different behavior under 3.1.x, transpose works on all versions

--- graphics/text/rotate.py
+++ graphics/text/rotate.py
@@ -9,8 +9,19 @@ import os
 
 img = Image.open(sys.argv[1])
 img.load()
-img2 = img.rotate(int(sys.argv[2]), 0, True)
-img2.crop() 
+angle = int(sys.argv[2])
+if angle % 90 == 0:
+    if angle == 90 or angle == -270:
+        method = Image.ROTATE_90
+    elif abs(angle) == 180:
+        method = Image.ROTATE_180
+    else:
+        method = Image.ROTATE_270
+    img2 = img.transpose(method)
+else:
+    img2 = img.rotate(int(sys.argv[2]), 0, True)
+    img2 = img2.crop()
+
 if os.path.exists(sys.argv[3]):  # delete any previous result file
 	os.remove(sys.argv[3])
 img2.save(sys.argv[3])
